985642ce6861cf6a5e20dc436d08867b48a2fba3
jeronstone <jstone14@terpmail.umd.edu>
2023-12-05 08:34:04 -0800

    library

--- xai_display/XAIDisplay.py
+++ xai_display/XAIDisplay.py
@@ -1,17 +1,78 @@
 
+from websocket_server import WebsocketServer
+import threading
+import json
+import webbrowser
+import os
+import random
 
-def send_iamge_and_text(image):
-    send exp image and text
+LOUD:False
 
-def send_image(image):
-    send exp image
+class XAIDisplay():
 
-def send_exp_text(text):
-    send exp text
+    ws_server: WebsocketServer
+
+    def __init__(self):
+        try:
+            self.ws_server=WebsocketServer(port=8888, host='')
+        except OSError as e:
+            print(e)
+            exit(1)
+        self.ws_server.set_fn_new_client(self.new_client)
+        threading.Thread(target=self.ws_server.run_forever, name='Local Server', daemon=True).start()
+        webbrowser.open(os.getcwd() + "/index.html")
     
-def send_status_text(0 or 1):
-    send status text
+    def new_client(client, server: WebsocketServer):
+        pass
 
+    def send_image_and_text(self, image, text):
+        to_send = {}
+        to_send["op"] = "imgntxt"
+        to_send["image_link"] = image
+        to_send["text_raw"] = text
+        self.send_to_display(to_send)
 
-# https://github.com/openai/whisper
-# https://pypi.org/project/SpeechRecognition/
+    def send_image(self, image):
+        to_send = {}
+        to_send["op"] = "img"
+        to_send["image_link"] = image
+        self.send_to_display(to_send)
+
+    def send_exp_text(self, text):
+        to_send = {}
+        to_send["op"] = "txt"
+        to_send["text_raw"] = text
+        self.send_to_display(to_send)
+        
+    def send_status_text(self, turn):
+        to_send = {}
+        to_send["op"] = "status"
+        if (turn == 0):
+            to_send["text_status"] = "Your Turn"
+        else:
+            to_send["text_status"] = "My Turn"
+    
+        self.send_to_display(to_send)
+
+    def send_to_display(self, to_send):
+        for client in self.ws_server.clients:
+            self.ws_server.send_message(client, json.dumps(to_send))
+        if (LOUD):
+            print("ws send info")
+            print(to_send)
+
+# todo probably move this
+# policy probs
+prob_good = .3
+prob_okay = .4
+prob_bad = .3
+
+def get_random_policy():
+    prob = random.random()
+    if (prob < prob_good):
+        doc="g"
+    elif (prob-prob_good < prob_okay):
+        doc="o"
+    else:
+        doc="b"
+    return doc
