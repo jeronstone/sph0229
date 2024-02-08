ac71cfec8d291bf8395407a98822bd28aadbfd34
jeronstone <jstone14@terpmail.umd.edu>
2024-02-08 10:13:13 -0800

    ballin ball ball ballin

--- exit_interviews/textify.py
+++ exit_interviews/textify.py
@@ -4,20 +4,21 @@ import sys
 
 print("----------")
 print(sys.argv)
-p = sys.argv[1]
+for i in range(1,len(sys.argv)):
+    p = sys.argv[i]
 
-command = "ffmpeg -i ~/exit_int_venv/P" + str(p) + ".AVI -ab 160k -ac 2 -ar 44100 -vn audioP" + str(p) + ".wav"
+    command = "ffmpeg -i ~/exit_int_venv/P" + str(p) + ".AVI -ab 160k -ac 2 -ar 44100 -vn audioP" + str(p) + ".wav"
 
-subprocess.call(command, shell=True)
+    subprocess.call(command, shell=True)
 
-print("start P " + str(p))
-model = whisper.load_model("medium")
-result = model.transcribe("audioP" + str(p) + ".wav")
-print(result["text"])
+    print("start P " + str(p))
+    model = whisper.load_model("medium")
+    result = model.transcribe("audioP" + str(p) + ".wav")
+    print(result["text"])
 
-f = open("raw+transcript_P" + str(p) + ".txt", "w")
-f.write(result["text"])
-f.close()
+    f = open("raw_transcript_P" + str(p) + ".txt", "w")
+    f.write(result["text"])
+    f.close()
 
-print("end P " + str(p))
-print("----------")
+    print("end P " + str(p))
+    print("----------")
